3c4ca6b8c5fd738b1a3178563d7b2726d73f0789
Santiago Alvarez <saalvarezva@gmail.com>
2017-01-20 23:49:59 -0500

    Initial Scene Setup

---
+++ Assets/Scripts/Catapult.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoundStep
+{
+	Pause,
+	Select,
+	Rotation,
+	Force,
+	Launch
+}
+
+public class Catapult : MonoBehaviour 
+{
+
+	public GameObject[] objectsToThrow;
+	public float changesPerSecond = 1.5f;
+
+	public RoundStep currentStep;
+
+
+	int currentIndexObject;
+
+	void Start()
+	{
+		currentStep = RoundStep.Pause;
+	}
+
+	public void OnTap()
+	{
+		switch(currentStep)
+		{
+		case RoundStep.Pause:
+			currentStep = RoundStep.Select;
+			OnStartSelection ();
+				break;
+
+		case RoundStep.Select:
+			currentStep = RoundStep.Rotation;
+			OnObjectSelected ();
+				break;
+
+		case RoundStep.Rotation:
+			currentStep = RoundStep.Force;
+			OnThrowObject ();
+			break;
+
+		case RoundStep.Force:
+			break;
+
+		case RoundStep.Launch:
+			break;
+		}
+	}
+
+	void OnStartSelection()
+	{
+		StartCoroutine ("OnSelectionChanger");
+	}
+
+	IEnumerator OnSelectionChanger()
+	{
+		currentIndexObject = Random.Range(0, objectsToThrow.Length);
+		objectsToThrow [currentIndexObject].SetActive (true);
+		while (true) 
+		{
+			yield return new WaitForSeconds (1/changesPerSecond);
+			objectsToThrow [currentIndexObject].SetActive (false);
+			currentIndexObject++;
+			if (currentIndexObject >= objectsToThrow.Length) 
+			{
+				currentIndexObject = 0;
+			}
+
+			objectsToThrow [currentIndexObject].SetActive (true);
+		}
+	}
+
+	void OnObjectSelected()
+	{
+		StopCoroutine ("OnSelectionChanger");
+	}
+
+	void OnThrowObject()
+	{
+		objectsToThrow [currentIndexObject].GetComponent<ObjectCatapult> ().LaunchObject ();
+	}
+}
---
+++ Assets/Scripts/ObjectToLaunch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectToLaunch : MonoBehaviour {
+
+	public Vector3 force;
+
+	Vector3 startV;
+	bool OnAir;
+	// Use this for initialization
+	void Start () {
+		startV = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void LaunchObject()
+	{
+		GetComponent<Rigidbody> ().isKinematic = false;
+		GetComponent<Rigidbody> ().AddForce (force, ForceMode.Impulse);
+		OnAir = true;
+	}
+
+	void OnCollisionEnter(Collision obj)
+	{
+		if (OnAir) 
+		{
+			transform.position = startV;	
+			OnAir = false;
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().rotation = Quaternion.identity;
+			GetComponent<Rigidbody> ().isKinematic = true;
+		}
+	}
+}
